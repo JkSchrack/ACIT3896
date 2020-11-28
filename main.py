eae3cbb9e022024f37dcf50de3729aa421ce2ed9
Justin <jkschrack@gmail.com>
2020-11-27 18:55:21 -0800

    Mockery of Data

--- main.py
+++ main.py
@@ -1,2 +1,51 @@
+import nurses
+import schedule
+
+
+def nurseRosterPopulator():
+    count = 0
+    for nurse in nurses:
+        nurseData = [nurse, 0, 0, 0]
+        nurseIndex[nurse] = count
+        count += 1
+        nurseRoster.append(nurseData)
+
+def availability(day, shift, tempNurses):
+    count = 0
+    for n in range(len(nurseRoster)):
+        nursesInfo = nurses[nurseRoster[n][0]]
+        nurse = nurseRoster[n]
+        if nurse[2] < 36 and nurse[3] == 0:
+            if day not in nursesInfo["prefDays"]:
+                nurse[1] += 3
+            if shift not in nursesInfo["prefShift"]:
+                nurse[1] += 2
+            nurse[1] += 1
+            tempNurses.append(nurse)
+            nurseIndex[nurse[0]] = count
+            count += 1
+    return tempNurses
+
+
+def week(day, shift, tempNurseRoster):
+    tempAvailableNurses = availability(day, shift, tempNurseRoster)
+    for i in range(len(days)):
+        if (shift == "Day"):
+            tempAvailableNurses = availability(day, shift, tempAvailableNurses)
+
+
+
 if __name__ == "__main__":
-    print("Hello World!");
+    nurses = nurses.nurses
+    schedule = schedule.schedule
+
+    nurseIndex = {}
+    nurseRoster = []
+    days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+    shifts = ['Day', 'Tues']
+
+    nurseRosterPopulator()
+
+    week("Mon", "Day", nurseRoster)
+
+    print("Hello World!")
---
+++ schedule.py
@@ -0,0 +1,38 @@
+schedule = {
+    'PointValue': 0,
+    'Mon': {
+            'PointValue': 0,
+            'Day': [],
+            'Night': [],
+    },
+    'Tues': {
+        'PointValue': 0,
+        'Day': [],
+        'Night': [],
+    },
+    'Wed': {
+        'PointValue': 0,
+        'Day': [],
+        'Night': [],
+    },
+    'Thur': {
+        'PointValue': 0,
+        'Day': [],
+        'Night': [],
+    },
+    'Fri': {
+        'PointValue': 0,
+        'Day': [],
+        'Night': [],
+    },
+    'Sat': {
+        'PointValue': 0,
+        'Day': [],
+        'Night': [],
+    },
+    'Sun': {
+        'PointValue': 0,
+        'Day': [],
+        'Night': [],
+    },
+}
